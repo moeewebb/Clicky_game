bfa2d28234fa43d8bdaafcfbb317099f30d57caf
Maurice Webb <mwebb.mw@gmail.com>
2019-11-10 12:27:29 -0500

    changed server to 5000

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res){
     res.send('welcome to express');
 });
 
-const port = 3000;
+const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
