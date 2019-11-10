0ed1ab39c84dc541026614bc0f5be098ff672751
Maurice Webb <mwebb.mw@gmail.com>
2019-11-10 12:06:44 -0500

    react running port 3000

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const 
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+
+    res.send('welcome to express');
+});
+
+const port = 3000;
+
+app.listen(port, () => `Server running on port ${port}`);
