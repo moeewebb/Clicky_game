2d9c00863efd03ecbd5900cd76d53c73e26e5171
Maurice Webb <mwebb.mw@gmail.com>
2019-11-11 00:03:39 -0500

    app still improving

--- Clicky-Game2/src/App.js
+++ Clicky-Game2/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import friends from "./friends.json";
+import React, { Component } from "react";
+import images from "../images.json";
 import logo from './logo.svg';
 import './App.css';
+import card from "../components/card";
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+   images : images
+ }; 
+render(){ 
+     return (
   <div className="jumbotron">
   <h1 className="display-4">Clicky Game</h1>
   <p className="lead">Click Here to Play a Fun Game today.</p>
   <hr className="my-4"/>
   <p>See options below.</p>
+ { 
+      images.map((cartoon, i) => (
+          <card image={cartoon.largeImageURL}    />
+        ))} 
   </div>
+
   );
+}
 }
 
 export default App;
