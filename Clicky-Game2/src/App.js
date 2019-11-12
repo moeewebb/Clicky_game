d3361834c539f0ec6cde26c9421f58561dac2766
Maurice Webb <mwebb.mw@gmail.com>
2019-11-12 15:50:06 -0500

    changed file structure

--- Clicky-Game2/src/App.js
+++ Clicky-Game2/src/App.js
@@ -1,29 +1,45 @@
-import React, { Component } from "react";
-import images from "../images.json";
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './images';
+// eslint-disable-next-line
 import './App.css';
-import card from "../components/card";
+import Card from './components/card.js';
 
 class App extends Component {
+    state = {
+      //  images 
+    };
+    render() {
+        return (
+            <div className="jum">
+                <h1 className="display-4">Clicky Game</h1>
+                <p className="lead">Click Here to Play a Fun Game today.</p>
+                <hr className="my-4"/>
+                <p>See options below.</p>
+            {
+                this.state.images.map((cartoon, i) => (
+                <Card image={cartoon.largeImageURL}/>
+              ))}
+            </div>
 
-  state = {
-   images : images
- }; 
-render(){ 
-     return (
-  <div className="jumbotron">
-  <h1 className="display-4">Clicky Game</h1>
-  <p className="lead">Click Here to Play a Fun Game today.</p>
-  <hr className="my-4"/>
-  <p>See options below.</p>
- { 
-      images.map((cartoon, i) => (
-          <card image={cartoon.largeImageURL}    />
-        ))} 
-  </div>
+        )
 
-  );
-}
+    }
 }
 
 export default App;
+
+ //handleClick = id => {
+
+ //};
+
+ ///handleIncrement = () => {
+
+ //};
+
+ //handleReset = () => {
+
+ //};
+
+ //handleShuffle = () => {
+
+ //};
